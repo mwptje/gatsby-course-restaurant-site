c84162b49f98313fc52a1e6915d65806161eaf7c
mvdvalk <46578571+mwptje@users.noreply.github.com>
2019-10-02 15:43:30 +0200

    up to the footer component but not including

--- src/components/HomePageComponents/QuickInfo.js
+++ src/components/HomePageComponents/QuickInfo.js
@@ -1,7 +1,44 @@
 import React, { Component } from "react"
+import { Section, Title, SectionButton } from "../../utils"
+import styled from "styled-components"
+import { styles } from "../../utils"
+import { Link } from "gatsby"
 
 export default class QuickInfo extends Component {
   render() {
-    return <div>content of quickinfo</div>
+    return (
+      <Section>
+        <Title message="let us tell you" title="our mission" />
+        <QuickInfoWrapper>
+          <p className="text">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus,
+            veritatis explicabo vitae, ipsum, officiis voluptatem laborum
+            aspernatur minima non culpa at ratione quod beatae impedit repellat?
+            At quos voluptatibus fuga sed molestias atque harum laborum? Cum
+            quas, quibusdam molestias sit incidunt praesentium minima, soluta
+            quis perspiciatis nobis hic, unde aut!
+          </p>
+          <Link to="/about" style={{ textDecoration: "none" }}>
+            <SectionButton style={{ margin: "2rem auto" }}>about</SectionButton>
+          </Link>
+        </QuickInfoWrapper>
+      </Section>
+    )
   }
 }
+
+const QuickInfoWrapper = styled.div`
+  width: 90%;
+  margin: 2rem auto;
+  .text {
+    line-height: 2rem;
+    color: ${styles.colors.mainGrey};
+    word-spacing: 0.2rem;
+  }
+  @media {
+    width: 70%;
+  }
+  @media (min-width: 992px) {
+    width: 60%;
+  }
+`
